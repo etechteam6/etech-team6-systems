1dd670df4e251f2d641f6ce7159073cdc6321a22
KingMoses33 <136907140+KingMoses33@users.noreply.github.com>
2023-11-05 22:54:57 -0500

    Update main.tf

--- target/systems/ec2/main.tf
+++ target/systems/ec2/main.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.24.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "server" {
+    source = "https://github.com/etechteam6/base-iac-tf/tree/main//modules/ec2"
+    num = 5
+}
